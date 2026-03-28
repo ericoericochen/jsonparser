6b77a0a33325bf9e47f859d8a65c7c59538d9afd
Eric Chen <deeplearningericchen@gmail.com>
2026-03-27 22:15:53 -0400

    parsing simple jsons

---
+++ cli.py
@@ -0,0 +1,35 @@
+import argparse
+
+from pprint import pprint
+
+from parser import Scanner, build_ast
+
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--json", type=str, required=True)
+args = argparser.parse_args()
+
+# parse json string into json
+json_path = args.json
+with open(json_path, "r") as f:
+    json_str = f.read()
+
+scanner = Scanner(json_str)
+tokens = scanner.scan_tokens()
+
+
+print("tokens: ")
+pprint(tokens)
+print("=" * 100)
+
+ast = build_ast(tokens)
+
+print("ast: ")
+pprint(ast)
+print("=" * 100)
+
+
+parsed_json = ast.eval()
+
+print("parsed: ")
+pprint(parsed_json)
--- parser.py
+++ parser.py
@@ -1,15 +1,25 @@
 from enum import Enum
-from typing import List, Optional
+from typing import List, Optional, Dict
 from dataclasses import dataclass
-
 from pprint import pprint
+from abc import ABC, abstractmethod
+
+
+JSON = Dict | List | str | int | float | bool
+
+# Tokens
 
 
 class TokenType(Enum):
+    # symbols
     LEFT_PARENTHESIS = "LEFT_PARENTHESIS"
     RIGHT_PARENTHESIS = "RIGHT_PARENTHESIS"
-    STRING = "STRING"
     COLON = "COLON"
+
+    # primitives
+    STRING = "STRING"
+    NUMBER = "NUMBER"
+    BOOLEAN = "BOOLEAN"
 
 
 @dataclass
@@ -17,6 +27,15 @@ class Token:
     type: TokenType
     lexme: str
     literal: Optional[object] = None
+
+
+def is_digit(char: str):
+    assert len(char) == 1
+    return char in "0123456789"
+
+
+def is_lowercase_alpha(char):
+    return "a" <= char <= "z"
 
 
 class Scanner:
@@ -47,12 +66,18 @@ class Scanner:
 
         if char == "{":
             self.tokens.append(Token(type=TokenType.LEFT_PARENTHESIS, lexme=char))
+        elif char == "}":
+            self.tokens.append(Token(type=TokenType.RIGHT_PARENTHESIS, lexme=char))
         elif char == ":":
             self.tokens.append(Token(type=TokenType.COLON, lexme=char))
-        elif char == "\n":
-            return
         elif char == '"':
             self.scan_string()
+        elif is_digit(char):
+            self.scan_number()
+        elif char in {"t", "f"}:
+            self.scan_boolean()
+        elif char == "\n":
+            return
 
     def scan_string(self):
         # stop at the char "
@@ -71,8 +96,41 @@ class Scanner:
             )
         )
 
-    def peek(self):
-        return self.source[self.current]
+    def scan_number(self):
+        # TODO: refactor below
+        while is_digit(self.peek()):
+            self.advance()
+
+        if self.peek() == "." and is_digit(self.peek(1)):
+            self.advance()
+            while is_digit(self.peek()):
+                self.advance()
+
+        lexme = self.get_lexme()
+
+        if "." in lexme:
+            literal = float(lexme)
+        else:
+            literal = int(lexme)
+
+        self.tokens.append(Token(type=TokenType.NUMBER, lexme=lexme, literal=literal))
+
+    def scan_boolean(self):
+        print("scanning boolean yo")
+        while is_lowercase_alpha(self.peek()):
+            self.advance()
+
+        lexme = self.get_lexme()
+        assert lexme in {"true", "false"}
+
+        literal = True if lexme == "true" else False
+        self.tokens.append(Token(type=TokenType.BOOLEAN, lexme=lexme, literal=literal))
+
+    def peek(self, n: int = 0):
+        return self.source[self.current + n]
+
+    def get_lexme(self):
+        return self.source[self.start : self.current]
 
     def scan_tokens(self):
         while not self.is_at_end():
@@ -82,12 +140,127 @@ class Scanner:
         return self.tokens
 
 
-if __name__ == "__main__":
-    json_path = "./examples/person.json"
-    with open(json_path, "r") as f:
-        json_str = f.read()
+# AST Nodes
 
-    scanner = Scanner(json_str)
-    tokens = scanner.scan_tokens()
+
+@dataclass
+class JSONNode(ABC):
+
+    @abstractmethod
+    def eval(self) -> JSON: ...
+
+
+@dataclass
+class JSONString:
+    value: str
+
+    def eval(self) -> JSON:
+        return self.value
+
+    # JSONString is hashable - implements __hash__ and __eq__
+    def __hash__(self):
+        return hash(self.value)
+
+    def __eq__(self, other: "JSONString"):
+        return self.value == other.value
+
+
+@dataclass
+class JSONNumber:
+    value: int | float
+
+    def eval(self) -> JSON:
+        return self.value
+
+
+@dataclass
+class JSONBoolean:
+    value: bool
+
+    def eval(self) -> JSON:
+        return self.value
+
+
+@dataclass
+class JSONDict:
+    fields: dict[JSONString, JSONNode]
+
+    def eval(self) -> JSON:
+        return {k.eval(): v.eval() for k, v in self.fields.items()}
+
+
+def is_primitive(token: Token):
+    return token.type in {TokenType.STRING, TokenType.NUMBER}
+
+
+def build_ast(tokens: list[Token]) -> JSONNode:
+    # print("building ast")
+    # pprint(tokens)
+
+    # base case: primitives string, number, boolean
+    if len(tokens) == 1:
+        token = tokens[0]
+
+        if token.type == TokenType.STRING:
+            return JSONString(value=token.literal)
+        elif token.type == TokenType.NUMBER:
+            return JSONNumber(value=token.literal)
+        elif token.type == TokenType.BOOLEAN:
+            return JSONBoolean(value=token.literal)
+
+    # parse json dict or list
+    if len(tokens) >= 2:
+        first_token = tokens[0]
+        last_token = tokens[-1]
+
+        if (
+            first_token.type == TokenType.LEFT_PARENTHESIS
+            and last_token.type == TokenType.RIGHT_PARENTHESIS
+        ):
+            return build_dict_ast(tokens)
+
+
+def build_dict_ast(tokens: list[Token]) -> JSONDict:
+    """
+    Builds a JSONDict AST.
+
+    Requires first token to be a left parenthesis and last token to be a right parenthesis.
+    """
+
+    i = 1
+    num_tokens = len(tokens)
+    fields: dict[str, JSONNode] = {}
+
+    while i < num_tokens - 1:
+        # make sure key and colon types are correct
+        key = tokens[i]
+        colon = tokens[i + 1]
+
+        assert key.type == TokenType.STRING
+        assert colon.type == TokenType.COLON
+
+        key = build_ast([key])
+
+        # parse value into JSONNode
+        value_tokens = [tokens[i + 2]]
+
+        # handle primitive values string, number, boolean
+        if value_tokens[0].type == TokenType.STRING:
+            value = build_ast(value_tokens)
+            i = i + 3  # advance to next key
+        elif value_tokens[0].type == TokenType.NUMBER:
+            value = build_ast(value_tokens)
+            i = i + 3
+        elif value_tokens[0].type == TokenType.BOOLEAN:
+            value = build_ast(value_tokens)
+            i = i + 3
+
+        fields[key] = value
+
+    return JSONDict(fields=fields)
+
+
+def parse(tokens: List[Token]) -> JSON:
+    print("parsing tokens into json")
 
     pprint(tokens)
